19d010d2434ae55ed956e656b4bc4012872b6d1d
nobrega <giovani.angelo@gmail.com>
2020-06-02 13:50:28 -0300

    Commit the gitignore and main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from src.utils import load_audio
 from sklearn.decomposition import FastICA, PCA
 from config import config
+import numpy as np
 
-print(config.DB_DIR)
-msg = load_audio("CTR-R11-NaoFam_ch1.wav")
+msg2 = load_audio("CTR-R11-NaoFam_ch2.wav")
+msg4 = load_audio("CTR-R11-NaoFam_ch4.wav")
 
-# if msg['message'] == 'OK':
-#     v = msg['file']
+if msg2['message'] == 'OK':
+    v2 = msg2['body'][1]
+    fs2 = msg2['body'][0]
 
-#     ica = FastICA(n_components=3)
-#     S_ = ica.fit_transform(v)  # Reconstruct signals
+    v4 = msg4['body'][1]
+    fs4 = msg4['body'][0]
 
-# else:
-#     print(msg)
+    sample_v2 = v2[13*fs2:40*fs2]
+    sample_v4 = v4[13*fs4:40*fs4]
+
+    X = np.array( [sample_v2, sample_v4] ).T
+    ica = FastICA(n_components=4)
+    S_ = ica.fit_transform(X)  # Reconstruct signals
+
+else:
+    print(msg)
